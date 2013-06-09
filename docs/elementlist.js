8487cce071594fcdb2d060388c679bdcf75e10d2
Kevin Hill <kevinkhill@gmail.com>
2013-06-08 20:38:53 -0700

    updated documentation

--- docs/elementlist.js
+++ docs/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","AreaChart"],["c","backgroundColor"],["c","chartArea"],["c","configOptions"],["c","Exception"],["c","Gcharts"],["c","hAxis"],["c","legend"],["c","LineChart"],["c","PieChart"],["c","textStyle"]];
+ApiGen.elements = [["c","AreaChart"],["c","backgroundColor"],["c","chartArea"],["c","configOptions"],["c","Exception"],["c","Gcharts"],["c","hAxis"],["c","legend"],["c","LineChart"],["c","LineChartEvents"],["c","PieChart"],["c","textStyle"],["c","tooltip"]];
