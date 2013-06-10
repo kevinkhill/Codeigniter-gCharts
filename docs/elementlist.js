c2044dd2307f1a3912fb8235b41a5dbe7884bc99
Kevin Hill <kevinkhill@gmail.com>
2013-06-10 08:30:10 -0700

    created branch for cloning into CI directly

--- docs/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","AreaChart"],["c","backgroundColor"],["c","chartArea"],["c","configOptions"],["c","Exception"],["c","Gcharts"],["c","hAxis"],["c","legend"],["c","LineChart"],["c","LineChartEvents"],["c","PieChart"],["c","textStyle"],["c","tooltip"]];
